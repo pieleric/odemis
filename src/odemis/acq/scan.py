d265c0289ff7df5d5d6681347b3dd823ee41c7de
Éric Piel <piel@delmic.com>
2025-06-19 14:18:25 +0200

    WIP vector scan

--- src/odemis/acq/scan.py
+++ src/odemis/acq/scan.py
@@ -2,10 +2,10 @@ import math
 from typing import Tuple, Dict, Any
 
 import numpy
-from SSSDConfig.sssdoptions import translation
 
 from odemis import model
 from odemis.model import MD_PIXEL_SIZE_COR, MD_ROTATION_COR, MD_POS_COR
+from odemis.util.img import mergeMetadata
 
 
 def generate_scan_vector(scanner: model.HwComponent,
@@ -35,16 +35,17 @@ def generate_scan_vector(scanner: model.HwComponent,
     # Compute the min/max limits of the scan area
 
     # Take into account the "border" around each pixel
-    pxs = (width[0] / res[0], width[1] / res[1])
-    lim_fov = ((roi[0] + pxs[0] / 2, roi[2] - pxs[0] / 2 ), # X
-               (roi[1] + pxs[1] / 2, roi[3] - pxs[1] / 2))  # Y
+    pxs_fov = (width[0] / res[0], width[1] / res[1])
+    lim_fov = ((roi[0] + pxs_fov[0] / 2, roi[2] - pxs_fov[0] / 2), # X
+               (roi[1] + pxs_fov[1] / 2, roi[3] - pxs_fov[1] / 2))  # Y
 
+    # compute the limits in pixel coordinates (in the scanner coordinates)
     lim_px = ((full_res[0] * (lim_fov[0][0] - 0.5), full_res[0] * (lim_fov[0][1] - 0.5)),
               (full_res[1] * (lim_fov[1][0] - 0.5), full_res[1] * (lim_fov[1][1] - 0.5)))
 
     # center of the RoI, in pixel coordinates, from the center of FoV
     translation = ((lim_px[0][0] + lim_px[0][1]) / 2,
-                   (lim_px[0][0] + lim_px[0][1]) / 2)
+                   (lim_px[1][0] + lim_px[1][1]) / 2)
 
     # Compute the margin (for the settle time, along X) in pixels
 
@@ -92,10 +93,45 @@ def generate_scan_vector(scanner: model.HwComponent,
         # Shift back from center
         scan_vector += numpy.array(translation)
 
-    # TODO: compute metadata
+    # Compute metadata
+    pxs1_fov = (1 / full_res[0], 1 / full_res[1])  # pixel size in the scanner coordinates
+    scale = (pxs_fov[0] / pxs1_fov[0], pxs_fov[1] / pxs1_fov[1])  # ration between the pixel size of the scanner and the pixels scanned
+    pxs1_m = scanner.pixelSize.value  # pixel size in m, at the current magnification
+    trans_m = (translation[0] * pxs1_m[0], -translation[1] * pxs1_m[1])  # translation in m, Y is inverted in physical coordinates
     md_cor = {
-        MD_PIXEL_SIZE_COR: (1, 1), # FIXME
+        MD_PIXEL_SIZE_COR: scale,
         MD_ROTATION_COR: rotation,
-        MD_POS_COR: translation,  # FIXME: * scanner.pixelSize.value
+        MD_POS_COR: trans_m,
     }
+    # TODO: the problem with computing MD_POS_COR now is that if the magnification is changed, then
+    # the actual translation (in meters) changes, although the metadata doesn't. One option would be
+    # to introduce a new metadata such as MD_POS_COR_IN_PX, which would have to be multiplied by the
+    # MD_PIXEL_SIZE (after correction), and would stay correct even when the magnification is updated.
     return scan_vector, margin, md_cor
+
+
+def vector_data_to_img(data: model.DataArray, res: Tuple[int, int], margin: int, md_cor: Dict[str, Any]) -> model.DataArray:
+    """
+    Convert a vector data array to an image DataArray.
+    :param data: DataArray of shape (N), corresponding to data acquired from the scanner during vector scan.
+    :param res: X/Y of the scanning area (in pixels)
+    :param margin: (0<=int): number of additional pixels to add at the beginning of each scanned line
+    :param md_cor:
+    :return:
+    """
+    assert data.ndim == 1
+
+    x, y = res
+    line_len = x + margin
+    data2d = numpy.reshape(data, (-1, line_len))  # Reshape to (Y, X+margin)
+    if data2d.shape[0] != y:
+        raise ValueError(f"Data array length {data.shape} does not match the expected number of lines.")
+
+    img = data2d[:, margin:]  # Remove the margin
+
+    # Update the metadata, so that the image is displayed at the right position
+    adjusted_md = data.metadata.copy()  # Copy to avoid modifying the original metadata
+    mergeMetadata(adjusted_md)  # First merge the possible correction metadata already in the raw data
+    mergeMetadata(adjusted_md, md_cor)
+    img = model.DataArray(img, metadata=adjusted_md)
+    return img

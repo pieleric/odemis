8287d50a50fadd4b6e0cd97e13bad3b53a05dcad
Éric Piel <piel@delmic.com>
2025-06-19 14:18:25 +0200

    WIP vector scan

--- src/odemis/acq/test/scan_test.py
+++ src/odemis/acq/test/scan_test.py
@@ -1,15 +1,24 @@
 import math
+import os
 import unittest
 import numpy as np
 import numpy.testing
+import odemis
+from fs.wrap import read_only
 
 from odemis.acq.scan import generate_scan_vector
 from odemis import model
+from odemis.util import testing
+
 
 class MockScanner:
     def __init__(self, shape, settle_time):
         self.shape = shape
         self.settleTime = settle_time
+        self.pixelSize = model.TupleContinuous((1e-6, 1e-6),
+                                               range=((1e-9, 1e-9), (1e-3, 1e-3)),
+                                               unit="m",
+                                               readonly=True)
 
 class TestGenerateScanVector(unittest.TestCase):
     def setUp(self):
@@ -39,6 +48,8 @@ class TestGenerateScanVector(unittest.TestCase):
         self.assertEqual(margin, 10)  # 10µs settle time / 1µs dwell time = 10 pixels margin
         self.assertIn(model.MD_PIXEL_SIZE_COR, md_cor)
         self.assertIn(model.MD_POS_COR, md_cor)
+        self.assertEqual(md_cor[model.MD_PIXEL_SIZE_COR], (1, 1))  # Max res, so no pixel size change
+        self.assertEqual(md_cor[model.MD_POS_COR], (0, 0))  # Full FoV, so no translation
         self.assertEqual(md_cor[model.MD_ROTATION_COR], rotation)
 
     def test_rotation(self):
@@ -99,6 +110,37 @@ class TestGenerateScanVector(unittest.TestCase):
             self.scanner, res, roi, rotation, dwell_time
         )
         self.assertEqual(margin, 50)
+
+CONFIG_PATH = os.path.dirname(odemis.__file__) + "/../../install/linux/usr/share/odemis/"
+SPARC2_HWSYNC_CONFIG = CONFIG_PATH + "sim/sparc2-nidaq-sim.odm.yaml"
+
+class TestVectorAcquisition(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        testing.start_backend(SPARC2_HWSYNC_CONFIG)
+
+        # Find CCD & SEM components
+        cls.ebeam = model.getComponent(role="e-beam")
+        cls.sed = model.getComponent(role="se-detector")
+        cls.cl = model.getComponent(role="cl-detector")
+        cls.spec = model.getComponent(role="spectrometer")
+
+    def test_2d_img_as_vector(self):
+        """
+        Test basic acquisition of a full FoV image, using the vector scanning
+        """
+        res = (768, 512)
+        roi = (0, 0, 1.0, 1.0)  # Full area of the scanner
+        rotation = 0.0  # rad
+        dwell_time = 1e-6  # s
+
+        scan_vector, margin, md_cor = generate_scan_vector(
+            self.scanner, res, roi, rotation, dwell_time
+        )
+
+
+        vector_data_to_img
 
 
 if __name__ == "__main__":

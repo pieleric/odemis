d77b4957defffa4ed1ce03af3d13c537ea4b9d7b
Éric Piel <piel@delmic.com>
2025-06-20 17:42:19 +0200

    WIP SPARC stream scan rotation

--- src/odemis/acq/test/scan_test.py
+++ src/odemis/acq/test/scan_test.py
@@ -3,10 +3,12 @@ import os
 import unittest
 import numpy as np
 import numpy.testing
+from odemis.dataio import hdf5
+
 import odemis
 from fs.wrap import read_only
 
-from odemis.acq.scan import generate_scan_vector
+from odemis.acq.scan import generate_scan_vector, vector_data_to_img
 from odemis import model
 from odemis.util import testing
 
@@ -72,7 +74,6 @@ class TestGenerateScanVector(unittest.TestCase):
                       (self.scanner.shape[1] - 1) / 2)
         numpy.testing.assert_almost_equal(scan_vector[0], [-half_width[1], half_width[0]])  # Center of the first pixel
 
-
     def test_margin_calculation(self):
         res = (768, 512)
         roi = (0, 0, 1.0, 1.0)  # Full area of the scanner
@@ -131,16 +132,38 @@ class TestVectorAcquisition(unittest.TestCase):
         Test basic acquisition of a full FoV image, using the vector scanning
         """
         res = (768, 512)
-        roi = (0, 0, 1.0, 1.0)  # Full area of the scanner
-        rotation = 0.0  # rad
+        #roi = (0, 0, 1.0, 1.0)  # Full area of the scanner
+        # rotation = 0  # rad
+        roi = (0.3, 0.3, 0.7, 0.7)
+        rotation = math.radians(30) # DEBUG
         dwell_time = 1e-6  # s
 
         scan_vector, margin, md_cor = generate_scan_vector(
-            self.scanner, res, roi, rotation, dwell_time
+            self.ebeam, res, roi, rotation, dwell_time
         )
 
+        self.ebeam.dwellTime.value = dwell_time
+        self.ebeam.scanPath.value = scan_vector
 
-        vector_data_to_img
+        raw_data = self.sed.data.get()
+
+        img = vector_data_to_img(raw_data, res, margin, md_cor)
+        self.assertEqual(img.shape, (res[1], res[0]))
+        self.assertIn(img.dtype, (numpy.uint16, numpy.int16))
+
+        img_md = img.metadata
+
+        ebeam_md = self.ebeam.getMetadata()
+        ebeam_pxs = self.ebeam.pixelSize.value
+        ebeam_max_res = self.ebeam.shape[:2]
+        ebeam_fov = ebeam_pxs[0] * ebeam_max_res[0], ebeam_pxs[1] * ebeam_max_res[1]
+        exp_pxs = (ebeam_fov[0] / res[0], ebeam_fov[1] / res[1])
+        testing.assert_tuple_almost_equal(img_md[model.MD_PIXEL_SIZE], exp_pxs)
+        testing.assert_tuple_almost_equal(img_md[model.MD_POS], ebeam_md[model.MD_POS])  # Full FoV, so no translation
+        self.assertEqual(img_md[model.MD_ROTATION], rotation)
+
+    # TODO: test with rotation
+    
 
 
 if __name__ == "__main__":

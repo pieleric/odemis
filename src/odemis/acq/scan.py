600d125cdc5f227c51c59fd6f9f2032708f71fef
Éric Piel <piel@delmic.com>
2025-08-04 23:49:29 +0200

    WIP refactor with scan vector

--- src/odemis/acq/scan.py
+++ src/odemis/acq/scan.py
@@ -1,5 +1,5 @@
 import math
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, Optional
 
 import numpy
 
@@ -12,7 +12,7 @@ def generate_scan_vector(scanner: model.HwComponent,
                          res: Tuple[int, int],
                          roi: Tuple[float, float, float, float],
                          rotation: float,
-                         dwell_time: float
+                         dwell_time: Optional[float]
                          ) -> Tuple[numpy.ndarray, int, Dict[str, Any]]:
     """
     Generate a scan vector for the given scanner with specified parameters.
@@ -21,12 +21,15 @@ def generate_scan_vector(scanner: model.HwComponent,
     :param res: X/Y of the scanning area (in pixels)
     :param roi: Region of interest as left, top, right, bottom (in ratio from the
     whole area of the emitter => between 0 and 1)
-    :param rotation: Rotation angle in radians, with the center of rotation corresponding to the center of the rectangle
-    :param dwell_time: Dwell time for each point in seconds
+    :param rotation: Rotation angle in radians, with the center of rotation corresponding to the
+    center of the rectangle
+    :param dwell_time: Dwell time for each point in seconds. Used only to computed the margin,
+    as its size depends on the dwell time. If None, the margin is set to 0.
     :return: A tuple containing the scan vector, margin pixel, and metadata.
-     * scan vector: (N, 2) array of points to scan, including the margin (used for the settle time)
+     * scan vector: (N, 2) array of points to scan. dtype is float. This includes the margin
+     (used for the settle time)
      * margin: (0<=int): number of additional pixels to add at the beginning of each scanned line
-     * metadata TODO
+     * metadata: correction metadata to apply on the received DataArray to display it correctly.
     """
     full_res = scanner.shape[:2]  # maximum resolution, obtained when scale=(1,1)
     width = (roi[2] - roi[0],
@@ -49,13 +52,16 @@ def generate_scan_vector(scanner: model.HwComponent,
 
     # Compute the margin (for the settle time, along X) in pixels
 
-    # settle_time is proportional to the size of the ROI (and =0 if only 1 px)
-    st = scanner.settleTime * (lim_px[0][1] - lim_px[0][0]) / (full_res[0] - 1)
-    # Round-up if settle time represents more than 1% of the dwell time.
-    # Below 1% the improvement would be marginal, and that allows to have
-    # tiny areas (eg, 4x4) scanned without the first pixel of each line
-    # being exposed twice more than the others.
-    margin = int(math.ceil(st / dwell_time - 0.01))
+    if dwell_time is None:
+        margin = 0
+    else:
+        # settle_time is proportional to the size of the ROI (and =0 if only 1 px)
+        st = scanner.settleTime * (lim_px[0][1] - lim_px[0][0]) / (full_res[0] - 1)
+        # Round-up if settle time represents more than 1% of the dwell time.
+        # Below 1% the improvement would be marginal, and that allows to have
+        # tiny areas (eg, 4x4) scanned without the first pixel of each line
+        # being exposed twice more than the others.
+        margin = int(math.ceil(st / dwell_time - 0.01))
 
     # Generate the scan vector, as a 3D array of shape (Y, X+margin, 2)
     # prepare an array of the right type
@@ -111,7 +117,10 @@ def generate_scan_vector(scanner: model.HwComponent,
     return scan_vector, margin, md_cor
 
 
-def vector_data_to_img(data: model.DataArray, res: Tuple[int, int], margin: int, md_cor: Dict[str, Any]) -> model.DataArray:
+def vector_data_to_img(data: model.DataArray,
+                       res: Tuple[int, int],
+                       margin: int,
+                       md_cor: Dict[str, Any]) -> model.DataArray:
     """
     Convert a vector data array to an image DataArray.
     :param data: DataArray of shape (N), corresponding to data acquired from the scanner during vector scan.
@@ -136,3 +145,31 @@ def vector_data_to_img(data: model.DataArray, res: Tuple[int, int], margin: int,
     mergeMetadata(adjusted_md, md_cor)
     img = model.DataArray(img, metadata=adjusted_md)
     return img
+
+def shift_scan_vector(scanner: model.HwComponent,
+                      scan_vector: numpy.ndarray,
+                      shift: Tuple[float, float],
+                      ) -> Tuple[numpy.ndarray, Tuple[float, float]]:
+    """
+    Shift the scan path by the given shift. Automatically clips the shift if it would cause the
+    scan path to go out of bounds (of the scanner range).
+    :param scanner: Scanner component
+    :param scan_vector: Scan vector to shift, as a (N, 2)
+    :param shift: Shift to apply, as a tuple (dx, dy) in the scanner coordinates
+    :return:
+     * shifted scan vector (within the scanner limits)
+     * shift, possibly clipped to the scanner limits
+    """
+    # Bounding box of the scan vector
+    min_x, min_y = numpy.min(scan_vector, axis=0)
+    max_x, max_y = numpy.max(scan_vector, axis=0)
+
+    limits = scanner.translation.range   # neg -> pos
+    # FIXME check
+    shift_range = ((limits[0][0] - min_x, limits[1][0] - max_x),
+                   (limits[0][1] - min_y, limits[1][1] - max_y))
+    clipped_shift = (min(max(shift_range[0][0], shift[0]), shift_range[0][1]),
+                     min(max(shift_range[1][0], shift[1]), shift_range[1][1]))
+    scan_vector = scan_vector + clipped_shift  # New numpy array
+
+    return scan_vector, clipped_shift

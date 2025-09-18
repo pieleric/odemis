00e3c57ddc557a13f1bf380e5a4b03e7a35d0689
Éric Piel <piel@delmic.com>
2025-09-18 15:33:37 +0200

    DEBUG: extra frameOverhead VA to test shadow spectra

--- src/odemis/gui/conf/data.py
+++ src/odemis/gui/conf/data.py
@@ -1022,6 +1022,8 @@ STREAM_SETTINGS_CONFIG = {
             ("filter", {  # from filter
                 "choices": util.format_band_choices,
             }),
+            ("frameOverhead", {
+            }),
         )),
     stream.AngularSpectrumSettingsStream:
         OrderedDict((
